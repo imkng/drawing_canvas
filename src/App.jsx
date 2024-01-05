4a5ca0fcaf36a657509c3d2997eedaf8a194a086
krnand <1india.kng@gmail.com>
2024-01-05 22:02:12 +0530

    implemented resize functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,91 @@ function createNewElement(id, x1, y1, x2, y2, elementType){
 
 const distance = (a,b) => Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
 
-const isWithinElements = (x, y, element)=>{
+const nearPoint = (x,y, x1, y1, name) =>{
+  return Math.abs(x - x1) < 5 && Math.abs(y - y1) < 5 ? name: null;
+};
+
+
+const positionWithinElements = (x, y, element)=>{
   const {elementType, x1, x2, y1, y2} = element;
   if(elementType == "rectangle"){
-    const minX = Math.min(x1, x2);
-    const maxX = Math.max(x1, x2);
-    const minY = Math.min(y1, y2);
-    const maxY = Math.max(y1, y2);
-    return x >= minX && x <= maxX && y >=minY && y <= maxY
+    const topLeft = nearPoint(x,y, x1, y1, "tl");
+    const topRight = nearPoint(x,y, x2, y1, "tr");
+    const bottomLeft = nearPoint(x,y, x1, y2, "bl");
+    const bottomRight = nearPoint(x,y, x2, y2, "br");
+    const inside =  x >= x1 && x <= x2 && y >=y1 && y <= y2 ? "inside": null;
+    return topLeft || topRight|| bottomLeft|| bottomRight || inside;
   }else{
     const a = {x: x1, y: y1};
     const b = {x: x2, y: y2};
     const c = {x, y};
     const offset = distance(a,b) - (distance(a,c) + distance(b,c));
-    return Math.abs(offset) < 1;
+    const start = nearPoint(x, y, x1, y1,"start");
+    const end = nearPoint(x, y, x2, y2,"end");
+    const inside =  Math.abs(offset) < 1 ? "inside": null;
+    return start || end || inside;
   }
 };
 
 
+const cursorForPosition = position => {
+  switch (position) {
+    case "tl":
+    case "br":
+    case "start":
+    case "end":
+      return "nwse-resize";
+    case "tr":
+    case "bl":
+      return "nesw-resize";
+    default:
+      return "move";
+  }
+};
+
 function getElementAtPosition (x, y,elements){
-  return elements.find(element => isWithinElements(x, y, element))
+return elements
+              .map(element => ({...element, position: positionWithinElements(x, y, element)}))
+              .find(element => element.position != null);
+}
+
+
+const adjustElementCoordinates = (element)=>{
+  const {elementType, x1, y1, x2, y2} = element;
+  if(elementType === "rectangle"){
+    const minX = Math.min(x1, x2);
+    const maxX = Math.max(x1, x2);
+    const minY = Math.min(y1, y2);
+    const maxY = Math.max(y1, y2);
+    return {x1: minX, y1: minY, x2: maxX, y2: maxY};
+  }else{
+    if(x1 < x2 || (x1 === x2 && y1 < y1)){
+      return {x1, y1, x2, y2};
+    }else{
+      return {x1: x2, y1: y2, x2: x1, y2: y1};
+    }
+  }
+};
+
+
+
+const resizeCoordinates = (clientX, clientY, position, coordinates) => {
+  const { x1, y1, x2, y2 } = coordinates;
+  switch (position) {
+    case "tl":
+    case "start":
+      return { x1: clientX, y1: clientY, x2, y2 };
+    case "tr":
+      return { x1, y1: clientY, x2: clientX, y2 };
+    case "bl":
+      return { x1: clientX, y1, x2, y2: clientY };
+    case "br":
+    case "end":
+      return { x1, y1, x2: clientX, y2: clientY };
+    default:
+      return null; //should not really get here...
+  }
+
 }
 
 
@@ -77,7 +142,13 @@ function App() {
         const offsetX = clientX - element.x1;
         const offsetY = clientY - element.y1;
         setSelectedElement({...element, offsetX, offsetY});
-        setAction("moving");
+
+        if(element.position === "inside"){
+          setAction("moving");
+        }else{
+          setAction("resize");
+        }
+        
       }
     }else{
       // console.log(clientX, clientY)
@@ -85,7 +156,8 @@ function App() {
       const newElement = createNewElement(id, clientX, clientY, clientX, clientY, tool);
       //useState
       setElements(prevState => [...prevState, newElement]);
-      console.log(elements);
+      setSelectedElement(newElement);
+      // console.log(elements);
       setAction('drawing');
     }
   };
@@ -95,7 +167,8 @@ function App() {
 
 
     if(tool === "selection"){
-      event.target.style.cursor = getElementAtPosition(clientX, clientY, elements) ? "move" : "default";
+      const element = getElementAtPosition(clientX, clientY, elements);
+      event.target.style.cursor = element ? cursorForPosition(element.position)  : "default";
     }
     if(action === 'drawing') {
       const index = elements.length - 1;
@@ -110,10 +183,20 @@ function App() {
       const newX1 = clientX - offsetX;
       const newY1 = clientY - offsetY;
       updateElement(id, newX1,newY1, newX1 + width, newY1 + height, elementType);
+    }if (action === "resize"){
+      const {id, elementType, position,  ...coordinates} = selectedElement;
+      const{x1, y1, x2, y2} = resizeCoordinates(clientX, clientY, position, coordinates);
+      updateElement(id, x1, y1, x2, y2, elementType);
     }
   };
 
   const handeMouseUp = (event) => {
+    const index = selectedElement.id;
+    const{id, elementType} = elements[index];
+    if(action === "drawing" || action === "resize"){
+      const {x1, y1, x2, y2} = adjustElementCoordinates(elements[index]);
+      updateElement(id, x1, y1, x2, y2, elementType);
+    }
     setAction('none');
     setSelectedElement(null);
   };
